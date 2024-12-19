115acfc13dc6ff0f31bf2f620e28ebf5a8132c2b
TheNabbu <thenabbu@users.noreply.github.com>
2024-12-19 23:12:07 +0530

    setting up templates and static folder

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
 from flask import Flask
 from markupsafe import escape
+from flask import url_for
+from flask import render_template
+
 
 app = Flask(__name__)
 
-@app.route("/<name>")
-def hello(name):
-    return f"Hello, {escape(name)}!"
+@app.route('/')
+def index():
+    return 'index'
+
+@app.route('/login')
+def login():
+    return 'login'
+
+@app.route('/user/<username>')
+def profile(username):
+    return f'{username}\'s profile'
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', person=name)
