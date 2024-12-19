316fe627c32c90c06daa0dafaaa5576522fabf79
TheNabbu <thenabbu@users.noreply.github.com>
2024-12-19 20:44:57 +0530

    main:

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-# This is a sample Python script.
+from typing import Union
 
-# Press Ctrl+F5 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from fastapi import FastAPI
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
+app = FastAPI()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
